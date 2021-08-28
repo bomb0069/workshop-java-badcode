322a0c416864d32a655a1d5afee04dc2a36b68ea
karan sivarat <bomb0069@gmail.com>
2021-08-28 13:57:18 +0700

    [Refactored]: extract validateEmail

--- src/main/java/badcode/RegisterBusiness.java
+++ src/main/java/badcode/RegisterBusiness.java
@@ -6,7 +6,9 @@ public class RegisterBusiness {
 
     public Integer register(SpeakerRepository repository, Speaker speaker) {
         Integer speakerId;
+
         validateSpeaker(speaker);
+        validateEmail(speaker.getEmail());
 
         int exp = speaker.getExp();
         speaker.setRegistrationFee(getFee(exp));
@@ -19,17 +21,23 @@ public class RegisterBusiness {
         return speakerId;
     }
 
-    private void validateSpeaker(Speaker speaker) {
+    private void validateEmail(String email) {
         String[] domains = {"gmail.com", "live.com"};
+        String emailDomain = getEmailDomain(email);
+        if (!contain(domains, emailDomain)) {
+            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
+        }
+    }
 
+    private boolean contain(String[] domains, String emailDomain) {
+        return Arrays.asList(domains).contains(emailDomain);
+    }
+
+    private void validateSpeaker(Speaker speaker) {
         if (isNullOrEmpty(speaker.getFirstName())) throw new ArgumentNullException("First name is required.");
         if (isNullOrEmpty(speaker.getLastName())) throw new ArgumentNullException("Last name is required.");
         if (isNullOrEmpty(speaker.getEmail())) throw new ArgumentNullException("Email is required.");
 
-        String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
-        if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() != 1) {
-            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
-        }
     }
 
     private boolean isNullOrEmpty(String value) {
