b37b9f92e05c5ab40f4d5076668d23f5ab733743
karan sivarat <bomb0069@gmail.com>
2021-08-28 10:45:04 +0700

    Add First Test Case

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -1,7 +1,25 @@
 package badcode;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterBusinessTest {
+
+    // Test To Pass
+
+    // Test To Fail
+    @Test
+    @DisplayName("ไม่ทำการกำหนดชื่อ จะเกิด Exception First name is required")
+    public void case01 () {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        try {
+            registerBusiness.register(null, new Speaker());
+            fail();
+        } catch (ArgumentNullException argumentNullException) {
+
+        }
+    }
 
 }
